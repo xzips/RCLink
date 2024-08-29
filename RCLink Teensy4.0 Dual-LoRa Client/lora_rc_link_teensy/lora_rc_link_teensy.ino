98c7483a47a334ca8882982be139e24e66359bd8
Mr_IO <aspenxe@gmail.com>
2024-08-29 14:26:07 -0400

    Painfully just got comm working with Teensy and just 1 lora

---
+++ RCLink Teensy4.0 Dual-LoRa Client/lora_rc_link_teensy/lora_rc_link_teensy.ino
@@ -0,0 +1,55 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  //
+  LoRa.setPins(9, 7, 5);
+
+  SPI.setSCK(13);
+  SPI.setMISO(12);
+  SPI.setMOSI(11);
+
+  //init spi
+  SPI.begin();
+
+  LoRa.setSPIFrequency(100000);
+
+  //test spi by sending data in a loop forever
+  //while (1) {
+  //  SPI.transfer(0x55);
+  //  //delay(1000);
+  //}
+  
+
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+
+
+
+    while (1);
+  }
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
